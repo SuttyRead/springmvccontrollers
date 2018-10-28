3c702c1d70ec5e0083460d6b445bc3c39bd81113
SuttyRead <SuttyRead@gmail.com>
2018-10-28 20:56:47 +0200

    Initial commit

---
+++ src/main/java/com/ua/sutty/config/ApplicationContextConfig.java
@@ -0,0 +1,20 @@
+package com.ua.sutty.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan("com.ua.sutty")
+public class ApplicationContextConfig {
+
+    @Bean(name = "viewResolver")
+    public InternalResourceViewResolver getViewResolver(){
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/pages/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+}
